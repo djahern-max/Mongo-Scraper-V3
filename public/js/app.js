bdb62714dea19f4ff69e081d8be81742e4025d87
Dano <danielaherniv@gmail.com>
2019-11-01 22:22:24 -0400

    working on click events

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,15 @@
-// Scrape Button
+//Save Button
+$(".save").on("click", function () {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/saved/" + thisId
+    }).then(function (data) {
+        window.location = "/"
+    })
+});
 
+// Scrape Button
 $("#scrape").on("click", function () {
     $.ajax({
         method: "GET",
@@ -7,5 +17,16 @@ $("#scrape").on("click", function () {
     }).then(function (data) {
         console.log(data)
         window.location = "/"
+    });
+});
+
+//Delete from Saved
+$('.delete').on('click', function () {
+    let thisId = $(this).attr('data-id');
+    $.ajax({
+        method: "POST",
+        url: '/delete/' + thisID
+    }).then(function (data) {
+        window.location = '/'
     });
 });
